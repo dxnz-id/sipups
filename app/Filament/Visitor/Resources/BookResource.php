38f5199e430c811332b9a6dfdf3045d6783819dd
dxnz-id <dxnzid@gmail.com>
2025-02-16 19:49:29 +0700

    add: book resource with card ui on visitor panel

---
+++ app/Filament/Visitor/Resources/BookResource.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Filament\Visitor\Resources;
+
+use App\Filament\Visitor\Resources\BookResource\Pages;
+use App\Models\Book;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\ContentGrid;
+use function Symfony\Component\Translation\t;
+
+class BookResource extends Resource
+{
+    protected static ?string $model = Book::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+
+    protected static ?string $navigationLabel = 'Books';
+
+    protected static ?string $navigationGroup = 'Library';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->label('Title'),
+                Forms\Components\TextInput::make('author')
+                    ->required()
+                    ->label('Author'),
+                Forms\Components\TextInput::make('publisher')
+                    ->required()
+                    ->label('Publisher'),
+                Forms\Components\DatePicker::make('published_at')
+                    ->required()
+                    ->label('Published At'),
+                Forms\Components\TextInput::make('isbn')
+                    ->required()
+                    ->unique(Book::class)
+                    ->label('ISBN (International Standard Book Number)'),
+                Forms\Components\Select::make('category_id')
+                    ->label('Category')
+                    ->options(Category::pluck('name', 'id'))
+                    ->searchable()
+                    ->required(),
+                Forms\Components\FileUpload::make('cover')
+                    ->image()
+                    ->label('Book Cover')
+                    ->minSize(4)
+                    ->maxSize(10240)
+                    ->previewable(false)
+                    ->required(),
+                Forms\Components\FileUpload::make('pdf_file')
+                    ->label('PDF File')
+                    ->acceptedFileTypes(['application/pdf'])
+                    ->minSize(4)
+                    ->maxSize(25600)
+                    ->required()
+                    ->previewable(false),
+                Forms\Components\RichEditor::make('description')
+                    ->label('Description')
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Split::make([
+                    ImageColumn::make('cover')
+                        ->label('Cover')
+                        ->width(150)
+                        ->height('auto'),
+                    Stack::make([
+                        TextColumn::make('title')
+                            ->label('Title')
+                            ->extraAttributes(['class' => 'font-bold'])
+                            ->searchable()
+                            ->sortable(),
+                        TextColumn::make('author')
+                            ->label('Author')
+                            ->sortable()
+                            ->searchable(),
+                        TextColumn::make('published_at')
+                            ->label('Publish Date')
+                            ->sortable()
+                            ->dateTime('Y'),
+                    ]),
+                ]),
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ])
+            ->filters([
+                SelectFilter::make('category_id')
+                    ->label('Category')
+                    ->options(Category::pluck('name', 'id'))
+                    ->searchable(),
+            ])
+            ->actions([
+                EditAction::make()->label('View Book')->icon('heroicon-o-book-open'),
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBooks::route('/'),
+            // 'create' => Pages\CreateBook::route('/create'),
+            // 'edit' => Pages\EditBook::route('/{record}/edit'),
+        ];
+    }
+}
